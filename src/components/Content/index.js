5cf3b47624c043b88c74f5f1e3ee2664f3019014
junior <junioralberto19@gmail.com>
2018-10-14 11:17:07 -0300

    chore(theme_color): change theme color

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -6,7 +6,7 @@ import Box from '../Box/Box.styled'
 const Content = () => (
   <ContentStyled>
     <Box>
-      <h1>Hello Peoples</h1>
+      <h1>Hello People</h1>
     </Box>
   </ContentStyled>
 )
